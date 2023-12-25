86cfb8eaae8e048f7a2ca9cfe70b9b39555d346a
pradyumna <pradyumnakaranth18@gmail.com>
2023-12-25 09:03:26 +0530

    commit

---
+++ src/main/java/com/example/codingquestion/CodingquestionApplication.java
@@ -0,0 +1,13 @@
+package com.example.codingquestion;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodingquestionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CodingquestionApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/codingquestion/CodingquestionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.codingquestion;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CodingquestionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
